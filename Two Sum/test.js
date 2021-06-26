25b4b8863a90d3747870b28565472f5c96938215
Alison Major <alison.major@gmail.com>
2021-06-26 12:57:17 -0400

    Write and pass third example test

--- Two Sum/test.js
+++ Two Sum/test.js
@@ -3,7 +3,15 @@ import { twoSum } from './index.js';
 
 describe('Two Sum', () => {
   
-  it('should return [0,1] for nums=[2,7,11,15] and target=9 (Example 1)');
+  it('should return [0,1] for nums=[2,7,11,15] and target=9 (Example 1)', () => {
+    const nums = [2,7,11,15];
+    const target = 9;
+    
+    const expected = [0,1];
+    const actual = twoSum(nums, target);
+
+    assert.deepStrictEqual(actual, expected);
+  });
 
   it('should return [1,2] for nums=[3,2,4] and target=6 (Example 2)', () => {
     const nums = [3,2,4];
