1794a600b83369e6fae786f0e7c6eaf315964d37
Alison Major <alison.major@gmail.com>
2021-06-26 12:56:35 -0400

    Write and pass second example test

--- Two Sum/index.js
+++ Two Sum/index.js
@@ -1,4 +1,20 @@
 export const twoSum = (nums, target) => {
-  const result = [0,1];
-  return result;
+  let indexOne = 0;
+  let indexTwo = 1;
+  
+  for (let i = indexTwo; i < nums.length; i += 1) {
+    const valueOne = nums[indexOne];
+    const valueTwo = nums[indexTwo];
+
+    if (valueOne + valueTwo === target) {
+      indexTwo = i;
+      break;
+    } else if (i === nums.length - 1 && indexOne !== nums.length - 2) {
+      i = indexTwo;
+      indexOne = indexTwo;
+      indexTwo += 1;
+    }
+}
+
+  return [indexOne, indexTwo];
 };
