fa4de3700a4402a1bef0303ca6107d1064866206
Alison Major <alison.major@gmail.com>
2021-06-26 12:11:42 -0400

    Create files for "Two Sum" problem

---
+++ Two Sum/test.js
@@ -0,0 +1,9 @@
+describe('Two Sum', () => {
+  
+  it('should return [0,1] for nums=[2,7,11,15] and target=9 (Example 1)');
+
+  it('should return [1,2] for nums=[3,2,4] and target=6 (Example 2)');
+
+  it('should return [0,1] for nums=[3,3] and target=6 (Example 3)');
+
+});
