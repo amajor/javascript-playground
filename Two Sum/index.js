887a3d8953d4966b72d0c00dc7c7cafd54077d7b
Alison Major <alison.major@gmail.com>
2021-06-26 12:40:34 -0400

    Write and pass smallest test

---
+++ Two Sum/index.js
@@ -0,0 +1,4 @@
+export const twoSum = (nums, target) => {
+  const result = [0,1];
+  return result;
+};
--- Two Sum/test.js
+++ Two Sum/test.js
@@ -1,9 +1,20 @@
+import assert from 'assert';
+import { twoSum } from './index.js';
+
 describe('Two Sum', () => {
   
   it('should return [0,1] for nums=[2,7,11,15] and target=9 (Example 1)');
 
   it('should return [1,2] for nums=[3,2,4] and target=6 (Example 2)');
 
-  it('should return [0,1] for nums=[3,3] and target=6 (Example 3)');
+  it('should return [0,1] for nums=[3,3] and target=6 (Example 3)', () => {
+    const nums = [3,3];
+    const target = 6;
+    
+    const expected = [0,1];
+    const actual = twoSum(nums, target);
+
+    assert.deepStrictEqual(actual, expected);
+  });
 
 });
