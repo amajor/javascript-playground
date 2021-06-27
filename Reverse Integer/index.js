5219154661a5fd68ca3fca20d56110b85eaa793a
Alison Major <alison.major@gmail.com>
2021-06-26 21:31:30 -0400

    Add variation to the solution

--- Reverse Integer/index.js
+++ Reverse Integer/index.js
@@ -22,6 +22,29 @@ const reverseInteger = (input) => {
   return parseInt(newNumberString, 10);
 };
 
+const reverseIntegerVariation = (input) => {
+  // Throw an error if the input is not an integer.
+  if (!Number.isInteger(input)) throw new Error('Input is not an integer.');
+
+  // Return 0 if the input is outside of the range limit.
+  const minInclusiveRange = -(2 ** 31); // -2 raised to the power of 31
+  const maxInclusiveRange = (2 ** 31) - 1; // 2 raised to the power of 31, minus 1
+  if (input < minInclusiveRange || input > maxInclusiveRange) return 0;
+
+  // Return 0 if the input is 0.
+  if (input === 0) return 0;
+
+  // Determine if its negative and store that as a multiplication factor.
+  const absoluteValue = Math.abs(input);
+  const factor = input / absoluteValue;
+
+  // Reverse the number.
+  const reversedNumber = parseInt(`${absoluteValue}`.split('').reverse().join(''), 10);
+
+  return reversedNumber * factor;
+};
+
 export {
   reverseInteger,
+  reverseIntegerVariation,
 };
