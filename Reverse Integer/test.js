e3acf7af14d14e1e38904f311cde6d47193ca651
Alison Major <alison.major@gmail.com>
2021-06-26 20:48:05 -0400

    Write remaining tests
    Our function already passes all of these!

--- Reverse Integer/test.js
+++ Reverse Integer/test.js
@@ -18,13 +18,37 @@ describe('Reverse Integer', () => {
     assert.strictEqual(actual, expected);
   });
 
-  it('should return 21 when given 120 (Example 3)');
+  it('should return 21 when given 120 (Example 3)', () => {
+    const input = 120;
+    const expected = 21;
+    const actual = reverseInteger(input);
 
-  it('should return 0 when given 120 (Example 4)');
+    assert.strictEqual(actual, expected);
+  });
 
-  it('should return 32 when given 231 - 1');
+  it('should return 0 when given 0 (Example 4)', () => {
+    const input = 0;
+    const expected = 0;
+    const actual = reverseInteger(input);
 
-  it('should return -132 when given -231');
+    assert.strictEqual(actual, expected);
+  });
+
+  it('should return 32 when given 231 - 1', () => {
+    const input = (231 - 1);
+    const expected = 32;
+    const actual = reverseInteger(input);
+
+    assert.strictEqual(actual, expected);
+  });
+
+  it('should return -132 when given -231', () => {
+    const input = (-231);
+    const expected = -132;
+    const actual = reverseInteger(input);
+
+    assert.strictEqual(actual, expected);
+  });
 
   it('should throw an error if the input is below the minimum inclusive value', () => {
     const minInclusiveRange = -231;
