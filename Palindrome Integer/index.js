96425f3b5bb85129be56dba7f1fda0219954580d
Alison Major <alison.major@gmail.com>
2021-06-26 22:14:44 -0400

    Set up test suite for variation on solution

--- Palindrome Integer/index.js
+++ Palindrome Integer/index.js
@@ -13,6 +13,34 @@ const palindromeInteger = (input) => {
   return input === reversedNumber;
 };
 
+const palindromeIntegerVariation = (input) => {
+  // Throw error if the input is outside of the range limit.
+  const minInclusiveRange = -(2 ** 31); // -2 raised to the power of 31
+  const maxInclusiveRange = (2 ** 31) - 1; // 2 raised to the power of 31, minus 1
+  if (input < minInclusiveRange || input > maxInclusiveRange) throw new Error('Input is outside of the range limit.');
+
+  // SPECIAL CASES:
+  // - When input < 0, input is not a palindrome.
+  // - If the last digit of the number is 0, in order to be a palindrome,
+  //   the first digit of the number also needs to be 0.
+  //   Only 0 satisfies this property.
+  if (input < 0 || (input % 10 === 0 && input !== 0) || !Number.isInteger(input)) return false;
+
+  let reversedNumber = 0;
+  let inputNumber = input;
+  while (inputNumber > reversedNumber) {
+    reversedNumber = (reversedNumber * 10) + (inputNumber % 10);
+    inputNumber /= 10;
+  }
+
+  // When the length is an odd number, we can get rid of the middle digit by reversedNumber/10
+  // When the input is 12321, at the end of the loop we get inputNumber = 12, reversedNumber = 123.
+  // The middle digit doesn't matter in palidrome (it will always equal to itself);
+  // we can simply get rid of it.
+  return inputNumber === reversedNumber || inputNumber === reversedNumber / 10;
+};
+
 export {
   palindromeInteger,
+  palindromeIntegerVariation,
 };
--- Palindrome Integer/test.js
+++ Palindrome Integer/test.js
@@ -1,5 +1,8 @@
 import assert from 'assert';
-import { palindromeInteger } from './index.js';
+import {
+  palindromeInteger,
+  palindromeIntegerVariation,
+} from './index.js';
 
 describe.only('Palindrome Integer', () => {
   it('should return true if the input is 121 (Example 1)', () => {
@@ -42,7 +45,7 @@ describe.only('Palindrome Integer', () => {
     assert.strictEqual(actual, expected);
   });
 
-  it('should return true if the input is 123454321', () => {
+  it('should return true if the input is 2147337412', () => {
     const input = 2147337412;
     const expected = true;
     const actual = palindromeInteger(input);
@@ -55,7 +58,7 @@ describe.only('Palindrome Integer', () => {
     const theString = 'hello world';
     const theArray = [0, 1.2, 'hellow world'];
     const theObject = {};
-    const theFunction = () => { console.log('hello world'); };
+    const theFunction = () => { console.log('hello world'); }; // eslint-disable-line
     const expected = false;
 
     assert.strictEqual(palindromeInteger(theFloat), expected);
@@ -81,6 +84,91 @@ describe.only('Palindrome Integer', () => {
     const input = maxInclusiveRange + 1;
     assert.throws(
       () => palindromeInteger(input),
+      /^Error: Input is outside of the range limit.*/,
+    );
+  });
+});
+
+describe.only('Palindrome Integer Variation', () => {
+  it('should return true if the input is 121 (Example 1)', () => {
+    const input = 121;
+    const expected = true;
+    const actual = palindromeIntegerVariation(input);
+
+    assert.strictEqual(actual, expected);
+  });
+
+  it('should return false if the input is -121 (Example 2)', () => {
+    const input = -121;
+    const expected = false;
+    const actual = palindromeIntegerVariation(input);
+
+    assert.strictEqual(actual, expected);
+  });
+
+  it('should return false if the input is 10 (Example 3)', () => {
+    const input = 10;
+    const expected = false;
+    const actual = palindromeIntegerVariation(input);
+
+    assert.strictEqual(actual, expected);
+  });
+
+  it('should return false if the input is -101 (Example 4)', () => {
+    const input = -101;
+    const expected = false;
+    const actual = palindromeIntegerVariation(input);
+
+    assert.strictEqual(actual, expected);
+  });
+
+  it('should return true if the input is 123454321', () => {
+    const input = 123454321;
+    const expected = true;
+    const actual = palindromeIntegerVariation(input);
+
+    assert.strictEqual(actual, expected);
+  });
+
+  it('should return true if the input is 2147337412', () => {
+    const input = 2147337412;
+    const expected = true;
+    const actual = palindromeIntegerVariation(input);
+
+    assert.strictEqual(actual, expected);
+  });
+
+  it('should return false if the input is not an integer', () => {
+    const theFloat = 1.2;
+    const theString = 'hello world';
+    const theArray = [0, 1.2, 'hellow world'];
+    const theObject = {};
+    const theFunction = () => { console.log('hello world'); }; // eslint-disable-line
+    const expected = false;
+
+    assert.strictEqual(palindromeIntegerVariation(theFloat), expected);
+    assert.strictEqual(palindromeIntegerVariation(theString), expected);
+    assert.strictEqual(palindromeIntegerVariation(theArray), expected);
+    assert.strictEqual(palindromeIntegerVariation(theObject), expected);
+    assert.strictEqual(palindromeIntegerVariation(theFunction), expected);
+    assert.strictEqual(palindromeIntegerVariation(null), expected);
+    assert.strictEqual(palindromeIntegerVariation(undefined), expected);
+  });
+
+  it('should throw an error if the input is below the minimum inclusive value', () => {
+    const minInclusiveRange = -(2 ** 31);
+    const input = minInclusiveRange - 1;
+    assert.throws(
+      () => palindromeIntegerVariation(input),
+      /^Error: Input is outside of the range limit.*/,
+    );
+  });
+
+  it('should throw an error if the input is above the maximum inclusive value', () => {
+    const maxInclusiveRange = (2 ** 31) - 1;
+    const input = maxInclusiveRange + 1;
+    assert.throws(
+      () => palindromeIntegerVariation(input),
       /^Error: Input is outside of the range limit.*/,
     );
   });
