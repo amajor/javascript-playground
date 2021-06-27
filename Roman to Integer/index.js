4bcc813e958afe8c18678988cfb4a7437757d802
Alison Major <alison.major@gmail.com>
2021-06-27 00:30:26 -0400

    Create and pass test for example 2

--- Roman to Integer/index.js
+++ Roman to Integer/index.js
@@ -10,7 +10,19 @@ const romanToInteger = (romanNumerals) => {
   // Throw an error if the input is longer than 15 characters.
   if (romanNumerals.length > 15) throw new Error('Cannot convert string longer than 15-characters.');
 
-  return 3;
+  // Count number of I's at beginning of the string.
+  const strArray = romanNumerals.split('');
+  let total = 0;
+  let ones = 0;
+  for (let i = 0; i < strArray.length; i++) {
+    if (strArray[i] === 'I') {
+      total += 1;
+      ones += 1;
+    }
+    if (strArray[i] === 'V') total = 5 - ones;
+  }
+
+  return total;
 };
 
 export {
--- Roman to Integer/test.js
+++ Roman to Integer/test.js
@@ -10,7 +10,14 @@ describe.only('Roman to Integer', () => {
     assert.strictEqual(actual, expected);
   });
 
-  it('should return 4 when provided with an input of `IV`.');
+  it('should return 4 when provided with an input of `IV`.', () => {
+    const romanNumerals = 'IV';
+    const expected = 4;
+    const actual = romanToInteger(romanNumerals);
+
+    assert.strictEqual(actual, expected);
+  });
+
   it('should return 9 when provided with an input of `IX`.');
   it('should return 58 when provided with an input of `LVIII`.');
   it('should return 1994 when provided with an input of `MCMXCIV`.');
